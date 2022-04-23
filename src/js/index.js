0030d1d4e8490d0af5f6008f49dc9994a194ff75
ajaythapliyal <ajaythapliyal1703@gmail.com>
2022-04-23 23:03:27 +0530

    select card effect

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,26 @@ function showModal() {
 
 function hideModal() {
     const modalContainer = document.querySelector('.modal-container');
+    const pledgeCards = document.querySelectorAll('.modal .pledge-card');
+    pledgeCards.forEach(pledgeCard => {
+        pledgeCard.classList.remove('active');
+        pledgeCard.querySelector('input').checked = false
+    })
     document.body.style.overflow = 'scroll';
     modalContainer.style.display = 'none';
+}
+
+function showPledgeInput(element) {
+    const pledgeCards = document.querySelectorAll('.modal .pledge-card');
+    pledgeCards.forEach(pledgeCard => {
+        if (pledgeCard == element) {
+            pledgeCard.classList.add('active');
+            pledgeCard.querySelector('input').checked = true
+        } else {
+            pledgeCard.classList.remove('active');
+            pledgeCard.querySelector('input').checked = false
+        }
+    })
 }
 
 document.addEventListener('click', event => {
