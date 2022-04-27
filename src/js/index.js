3f389cd14417cd32c6c3d2d648ce9eade367c17c
ajaythapliyal <ajaythapliyal1703@gmail.com>
2022-04-27 07:54:57 +0530

    refreshes the dom when data changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,33 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-unused-vars */
-function showModal() {
+
+const fundingNode = document.getElementById('funding-info')
+const backersNode = document.getElementById('backers-info')
+const bambooQtyNodes = document.getElementsByClassName('bamboo-qty')
+const blackQtyNodes = document.getElementsByClassName('black-qty')
+const mahoganyQtyNodes = document.getElementsByClassName('mahogany-qty')
+const noPledgeCard = document.getElementById('noPledgeCard')
+const bambooPledgeCard = document.getElementById('bambooPledgeCard')
+const blackPledgeCard = document.getElementById('blackPledgeCard')
+const mahoganyPledgeCard = document.getElementById('mahoganyPledgeCard')
+const bambooInput = document.getElementById('bamboo-input')
+const blackInput = document.getElementById('black-input')
+const mahoganyInput = document.getElementById('mahogany-input')
+const backersInfo = document.getElementById('backers-info')
+const fundingInfo = document.getElementById('funding-info')
+
+function showModal(cardId) {
     const modalContainer = document.querySelector('.modal-container')
     document.body.style.overflow = 'hidden'
     modalContainer.style.display = 'block'
     modalContainer.scrollTop = '0px'
+    if (cardId) {
+        const card = document.getElementById(cardId)
+        card.classList.add('active')
+        document.getElementById(cardId).scrollIntoView()
+    }
     event.stopPropagation()
 }
 
@@ -16,6 +38,8 @@ function hideModal() {
         pledgeCard.classList.remove('active')
         const input = pledgeCard.querySelector('input')
         input.checked = false
+        const pledgeInput = pledgeCard.querySelector('.pledge-input')
+        pledgeInput.style.display = 'none'
     })
     document.body.style.overflow = 'scroll'
     modalContainer.style.display = 'none'
@@ -34,6 +58,8 @@ function showPledgeInput(element) {
             pledgeCard.classList.remove('active')
             const input = pledgeCard.querySelector('input')
             input.checked = false
+            const numInput = pledgeCard.querySelector('.input1')
+            if (numInput) numInput.classList.remove('error')
             const pledgeInput = pledgeCard.querySelector('.pledge-input')
             pledgeInput.style.display = 'none'
         }
@@ -46,27 +72,88 @@ document.addEventListener('click', (event) => {
     }
 })
 
-const store = (() => ({
+const _store = {
     backers: 5007,
     fundingGoal: 100000,
     fundingMet: 89914,
     products: {
-        1: { name: 'Bamboo Stand', qty: 101 },
-        2: { name: 'Black Edition Stand', qty: 64 },
-        3: { name: 'Mahogany Special Edition', qty: 0 },
+        1: { name: 'Bamboo Stand', qty: 101, amt: 25 },
+        2: { name: 'Black Edition Stand', qty: 64, amt: 75 },
+        3: { name: 'Mahogany Special Edition', qty: 0, amt: 200 },
     },
-    back(productId, amount) {
-        if (productId) {
-            this.products[productId].qty -= 1
-        }
-        this.backers += 1
-        if (amount) this.fundingMet += amount
-    },
-}))()
+}
 
-const project = new Proxy(store.back, {
+function refreshBackers() {
+    backersInfo.innerHTML = _store.backers
+}
+
+function refreshFunding() {
+    fundingInfo.innerHTML = _store.fundingMet
+}
+
+const store = new Proxy(_store, {
+    set(obj, prop, val) {
+        obj[prop] = val
+        if (prop === 'backers') {
+            refreshBackers()
+        } else if (prop === 'fundingMet') {
+            refreshFunding()
+        }
+    },
+})
+
+function back(productId, amount) {
+    if (!productId && !amount) {
+        store.backers += 1
+        return true
+    }
+    const product = store.products[productId]
+    const amtNum = Number(amount)
+    if (amtNum && amtNum >= product.amt) {
+        product.qty -= 1
+        store.backers += 1
+        store.fundingMet += amtNum
+        return true
+    }
+    return false
+}
+
+const backProject = new Proxy(back, {
     apply(target, that, args) {
-        const response = target.call(that, ...args)
+        const response = target(...args)
+        if (response) {
+            console.log('validation passed')
+        } else {
+            ;[...args[2].parentNode.childNodes]
+                .filter((node) => node.id)
+                .forEach((node) => node.classList.add('error'))
+        }
         return response
     },
 })
+
+fundingNode.innerHTML = store.fundingMet
+backersNode.innerHTML = store.backers
+
+for (let index = 0; index < bambooQtyNodes.length; index += 1) {
+    bambooQtyNodes[index].childNodes[0].textContent = store.products[1].qty
+    if (store.products[1].qty === 0) {
+        bambooQtyNodes[index].closest('.card').classList.add('disabled')
+    }
+}
+
+for (let index = 0; index < blackQtyNodes.length; index += 1) {
+    blackQtyNodes[index].childNodes[0].textContent = store.products[2].qty
+    if (store.products[1].qty === 0) {
+        blackQtyNodes[index].closest('.card').classList.add('disabled')
+    }
+}
+
+for (let index = 0; index < mahoganyQtyNodes.length; index += 1) {
+    mahoganyQtyNodes[index].childNodes[0].textContent = store.products[3].qty
+    if (store.products[3].qty === 0) {
+        const card = mahoganyQtyNodes[index].closest('.pledge-card')
+        card.classList.add('disabled')
+        card.style.pointerEvents = 'none'
+    }
+}
